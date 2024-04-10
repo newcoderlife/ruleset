b44a7e28d1b6ac368f70f3ac3a9f303f30b4e9bc
newco <newco>
2024-04-10 16:17:46 +0800

    feat: Add bash script

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+set -ex
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <path_to_log_file>"
+    exit 1
+fi
+
 if [ ! -f .env/bin/activate ]; then
     python3 -m venv .env
 fi
@@ -7,4 +14,4 @@ fi
 source .env/bin/activate
 pip install -r requirements.txt
 
-./generate.py --log_file /var/log/coredns.log
+./generate.py --log_file $1
